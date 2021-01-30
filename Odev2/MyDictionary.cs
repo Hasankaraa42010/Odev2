a7f5499546c58dd778e1ce48c3b7402637b44119
Hasan Kara <hsn33y2@gmail.com>
2021-01-30 11:58:39 +0300

    Add project files.

---
+++ Odev2/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev2
+{
+    class MyDictionary<kutuphane,sınıf>
+    {
+        kutuphane[] elemanlar;
+        sınıf[] ogrenciler;
+        //constuctor
+        public MyDictionary()
+        {
+            elemanlar = new kutuphane[0];
+            ogrenciler = new sınıf[0];
+        }
+        public void Add(kutuphane eleman,sınıf ogrenci)
+        {
+            kutuphane[] geciciDizi = elemanlar;
+            sınıf[] geciciDizi2 = ogrenciler;
+            elemanlar = new kutuphane[elemanlar.Length + 1];
+            ogrenciler = new sınıf[ogrenciler.Length + 1];
+            for (int i = 0; i < geciciDizi.Length; i++)
+            {
+                elemanlar[i] = geciciDizi[i];
+            }
+            for (int i = 0; i < geciciDizi2.Length; i++)
+            {
+                ogrenciler[i] = geciciDizi2[i];
+            }
+            ogrenciler[ogrenciler.Length - 1] = ogrenci;
+            elemanlar[elemanlar.Length - 1] = eleman;
+        }
+        public int Length
+        {
+            get { return elemanlar.Length; }
+            
+        }
+        public int Lenght2
+        {
+            get { return ogrenciler.Length; }
+        }
+        public sınıf [] Ogrenciler
+        {
+            get { return ogrenciler; }
+        }
+        public kutuphane[] Elemanlar
+        {
+            get { return elemanlar; }
+        }
+    }
+}
---
+++ Odev2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Odev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> kutuphanem = new MyDictionary<string, string>();
+            kutuphanem.Add("Hasan Kara", "mehmet");
+
+            kutuphanem.Add("Hasan 2 Karaa", "ali");
+            Console.WriteLine("Eleman ismi=="+kutuphanem.Elemanlar[0]+" Ogrenci İsmi== "+kutuphanem.Ogrenciler[0]);
+            Console.WriteLine("Eleman ismi==" + kutuphanem.Elemanlar[1] + " Ogrenci İsmi== " + kutuphanem.Ogrenciler[1]);
+
+
+
+        }
+    }
+}
